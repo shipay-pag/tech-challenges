31543b7d55e3f8dd549b7afc23d77fc9bb201950
zero <luc4s.rib3iro@gmail.com>
2026-06-26 18:05:42 -0300

    feature: implement product listing and styling in App component

--- front_end/shipay-react-app/App.js
+++ front_end/shipay-react-app/App.js
@@ -2,7 +2,21 @@ import initialItems from './data'; // Mock data
 import './App.css';
 
 function App() {
-  
+  return (
+    <main className="app">
+      <section className="product-section" aria-labelledby="product-list-title">
+        <h1 id="product-list-title">Produtos transacionais Shipay</h1>
+
+        <ul className="product-list">
+          {initialItems.map((item) => (
+            <li className="product-list-item" key={item.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- front_end/shipay-react-app/data.js
+++ front_end/shipay-react-app/data.js
@@ -1,1 +1,9 @@
-[{ id: 1, name: 'Cash In - COB' }, { id: 2, name: 'Cash In - COBV' }, { id: 3, name: 'Cash In - DUEDATE' }, { id: 4, name: 'Cash In - CHARGE' }, { id: 5, name: 'Cash Out - PAYMENT' }];
+const initialItems = [
+  { id: 1, name: 'Cash In - COB' },
+  { id: 2, name: 'Cash In - COBV' },
+  { id: 3, name: 'Cash In - DUEDATE' },
+  { id: 4, name: 'Cash In - CHARGE' },
+  { id: 5, name: 'Cash Out - PAYMENT' },
+];
+
+export default initialItems;
